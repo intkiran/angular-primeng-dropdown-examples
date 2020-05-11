66353e8e3d2dc2f0d92510828716e776c8c6266d
intkiran <ikiranbabu@gmail.com>
2020-05-11 21:31:37 +0530

    3 files modified

--- src/app/basic-dropdown/basic-dropdown.component.ts
+++ src/app/basic-dropdown/basic-dropdown.component.ts
@@ -1,33 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { SelectItem } from 'primeng';
+import { Component, OnInit } from "@angular/core";
+import { SelectItem } from "primeng";
 
 @Component({
-  selector: 'app-basic-dropdown',
-  templateUrl: './basic-dropdown.component.html',
-  styleUrls: ['./basic-dropdown.component.css']
+  selector: "app-basic-dropdown",
+  templateUrl: "./basic-dropdown.component.html",
+  styleUrls: ["./basic-dropdown.component.css"]
 })
-
 export class BasicDropdownComponent implements OnInit {
   employes: SelectItem[];
   selectedEmploye: SelectItem;
 
-
-  constructor() {     
+  constructor() {
     this.employes = [
-      {label:'Select Employee', value:null},
-      {label:'Franc', value:1},
-      {label:'Kiran', value:2},
-      {label:'John', value:3},
+      { label: "Select Employee", value: null },
+      { label: "Franc", value: 1 },
+      { label: "Kiran", value: 2 },
+      { label: "John", value: 3 }
     ];
-        this.empList = [
-      {name:'Select Employee', id:null},
-      {name:'Franc', id:1},
-      {name:'Kiran', id:2},
-      {name:'John', id:3},
-    ];
-}
-
-  ngOnInit() {
   }
 
+  ngOnInit() {}
 }
