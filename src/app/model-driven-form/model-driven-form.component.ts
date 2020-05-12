28ee5250c24e9a665c5dd35db9ae89939f9288f0
intkiran <ikiranbabu@gmail.com>
2020-05-12 14:11:13 +0530

    1 file modified, 1 added

--- src/app/model-driven-form/model-driven-form.component.ts
+++ src/app/model-driven-form/model-driven-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng';
 
 @Component({
   selector: 'app-model-driven-form',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./model-driven-form.component.css']
 })
 export class ModelDrivenFormComponent implements OnInit {
+ roles: SelectItem[];
+  selectedRole: SelectItem;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.roles = [
+      { label: "Select Role", value: null },
+      { label: "Admin", value: 1 },
+      { label: "SuperAdmin", value: 2 },
+      { label: "User", value: 3 }
+    ];
   }
 
+  ngOnInit() {}
 }
